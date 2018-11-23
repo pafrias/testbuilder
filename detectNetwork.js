7369e80f41409c288fd1fac90d0ac6700d2d19d2
pafrias <36284288+pafrias@users.noreply.github.com>
2018-11-23 09:46:54 -0800

    Update detectNetwork.js

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,8 +11,14 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+  if (cardnumber.length === 14 && (cardnumber.slice[0,2] === 38 || cardnumber.slice[0, 2] === 39) {
+    return 'Diner\'s Club';
+  }  else if (cardnumber.length == 15 && (cardnumber.slice[0,2] === 34 || cardnumber.slice[0,2] === 37) {
+    return 'American Express';
+  } else {
+    return 'Please Try Again';
+  }
+  
 };
 
 
