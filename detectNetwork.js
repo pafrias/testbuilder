1e1d92329a9218401b33602a0174f1811b64e84d
pafrias <36284288+pafrias@users.noreply.github.com>
2018-11-23 12:52:52 -0800

    Update detectNetwork.js

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,26 +1,28 @@
-  // Should check the cardnumber, weed out entries that contain non-numbers
-  // Should check that number for which financial institution it belongs to
-  // should return a string with the name of that institution
+  // Note: `cardNumber` will always be a string
+  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
+  // The American Express network always starts with a 34 or 37 and is 15 digits long
 
-var testInt = (numbers) => { //checks for numbers only, also lets us work with non-strings if need be
-  let numbers = numbers.stringify();
-  let numArray = numbers.split();
-  return numArray.filter(num => (num.isNum()));
-}
-
-var detectNetwork = function(cardNumber) {
-  let cardNumber = testInt(cardNumber);
-  if (cardNumber.length === 14 && (cardNumber.slice(0,2) === '38' || cardNumber.slice(0,2) === '39')) {
+var detectNetwork = function(cardNumber) { //stringifies, checks for valid credit cards
+  cardNumber = cardNumber.toString();
+  if (testNotInt(cardNumber)) {
+    return 'Please enter only numbers';
+  }  else if (cardNumber.length === 14 && (cardNumber.slice(0,2) === '38' || cardNumber.slice(0,2) === '39')) {
     return 'Diner\'s Club';
   }  else if (cardNumber.length == 15 && (cardNumber.slice(0,2) === '34' || cardNumber.slice(0,2) === '37')) {
     return 'American Express';
-  } else {
-    return 'Please Try Again';
+  }  else {
+    return 'Invalid Card Number';
   }
 };
 
-testNum = '38145824658298';
-testNum2 = '374572169854265';
-testNums = [124567, ]
-console.log(testNum.slice(0,2));
-console.log(detectNetwork(testNum.length)
+function testNotInt(numbers) { // checks for non-numbers, then returns false for all numbers
+  let numArray = numbers.split('');
+  return (numArray.some(num => isNaN(parseInt(num))));
+};
+
+/*
+testNums = [124567, 38145824658298,'3814582be4658298','374572169854265', 124567, '38145824658298'];
+
+for (var iter = 0; iter < 6; iter++) {
+  console.log(detectNetwork(testNums[iter]));
+}*/
