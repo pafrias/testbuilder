fb77596050db284a45308212dd1e889120d4b044
pafrias <36284288+pafrias@users.noreply.github.com>
2018-11-23 12:02:16 -0800

    Update detectNetwork.js

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,24 +1,26 @@
-// Given a credit card number, this function should return a string with the 
-// name of a network, like 'MasterCard' or 'American Express'
-// Example: detectNetwork('343456789012345') should return 'American Express'
+  // Should check the cardnumber, weed out entries that contain non-numbers
+  // Should check that number for which financial institution it belongs to
+  // should return a string with the name of that institution
 
-// How can you tell one card network from another? Easy! 
-// There are two indicators:
-//   1. The first few numbers (called the prefix)
-//   2. The number of digits in the number (called the length)
+var testInt = (numbers) => { //checks for numbers only, also lets us work with non-strings if need be
+  let numbers = numbers.stringify();
+  let numArray = numbers.split();
+  return numArray.filter(num => (num.isNum()));
+}
 
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-  if (cardnumber.length === 14 && (cardnumber.slice[0,2] === 38 || cardnumber.slice[0, 2] === 39) {
+  let cardNumber = testInt(cardNumber);
+  if (cardNumber.length === 14 && (cardNumber.slice(0,2) === '38' || cardNumber.slice(0,2) === '39')) {
     return 'Diner\'s Club';
-  }  else if (cardnumber.length == 15 && (cardnumber.slice[0,2] === 34 || cardnumber.slice[0,2] === 37) {
+  }  else if (cardNumber.length == 15 && (cardNumber.slice(0,2) === '34' || cardNumber.slice(0,2) === '37')) {
     return 'American Express';
   } else {
     return 'Please Try Again';
   }
-  
 };
 
-
+testNum = '38145824658298';
+testNum2 = '374572169854265';
+testNums = [124567, ]
+console.log(testNum.slice(0,2));
+console.log(detectNetwork(testNum.length)
